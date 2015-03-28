a3df2311c2b2e72d69d6b3ad0f84ca39b32fb9d3
Dani Rodríguez <danirod@outlook.com>
2015-03-28 23:10:06 +0100

    Split register test case into two test cases.
    The test case still tests the same: the fact that if you update one
    of the fields of union register_t, the other field will be updated
    too.

--- tests/cpu_test.c
+++ tests/cpu_test.c
@@ -3,34 +3,20 @@
 #include "cpu.h"
 #include "cpu_test.h"
 
-START_TEST(test_register_wordbyte)
+START_TEST(register_t_test_word)
 {
     union register_t reg;
-    
-    /*
-     * When assigning a 16 bit value, upper and lower 8 bit subregisters
-     * should be properly splitted. This depends on whether host CPU
-     * architecture is big endian or little endian.
-     */
     reg.WORD = 0x1234;
     ck_assert(reg.WORD == 0x1234);
     ck_assert(reg.BYTES.H == 0x12);
     ck_assert(reg.BYTES.L == 0x34);
+}
+END_TEST
 
-    /*
-     * If we update one of the 8 bit subregister, 16 bit value should
-     * be updated. First we will test upper 8 bit subregister. We expect
-     * 8 most significant bits of word register to change.
-     */
+START_TEST(register_t_test_bytes)
+{
+    union register_t reg;
     reg.BYTES.H = 0x56;
-    ck_assert(reg.WORD == 0x5634);
-    ck_assert(reg.BYTES.H == 0x56);
-    ck_assert(reg.BYTES.L == 0x34);
-    
-    /*
-     * In the same fashion we can expect 8 least significant bits of word
-     * register to change when we update the other subregister.
-     */
     reg.BYTES.L = 0x78;
     ck_assert(reg.WORD == 0x5678);
     ck_assert(reg.BYTES.H == 0x56);
@@ -42,7 +28,8 @@ Suite*
 gensuite_cpu(void)
 {
     TCase* tc_register = tcase_create("Register");
-    tcase_add_test(tc_register, test_register_wordbyte);
+    tcase_add_test(tc_register, register_t_test_word);
+    tcase_add_test(tc_register, register_t_test_bytes);
 
     Suite* s = suite_create("CPU");
     suite_add_tcase(s, tc_register);
